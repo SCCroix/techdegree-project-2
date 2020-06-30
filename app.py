88d8127e464c51bc8c4ae071808adcfd28466605
Spencer <spencerccroy@gmail.com>
2020-06-30 16:20:56 -0700

    Add convert height and experience

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import constants
+import logging
+
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
 def convert_height_to_int(str):
     # Convert the text height to an integer
-    pass
+    return int(str.split()[0])
+
+def convert_exp_to_bool(str):
+    # Converts 'YES' or 'NO' to true false
+    return str == "YES"
+    
 
 def display_menu():
     print("\n ____ Menu ____ \n")
@@ -16,19 +24,20 @@ def clean_player_data(players):
     clean_data = []
     #iterate through the list of players
     for player in players:
-      # for each player iterate through key value pairs
-      clean_player_data = {}
-      for key, value in player:
-          pass
-          # Copy over name
-          # Change Guardians to list
-          # Change height to an integer
-    clean_data.append(clean_player_data)
-return clean_data
+        # for each player iterate through key value pairs
+        clean_player_data = {}
+        # Copy over name
+        clean_player_data['name'] = player['name']
+        # Change Guardians to list
+        # Change height to an integer
+        clean_player_data['height'] = convert_height_to_int(player['height'])
+        # Change experienced to true false
+        clean_player_data['experience'] = convert_exp_to_bool(player['experience'])
+        clean_data.append(clean_player_data)
+    logging.debug(clean_data)
+    return clean_data
 
-
-
-
+clean_player_data(constants.PLAYERS)
 
 if __name__ == "__main__":
     print("Basketball Team States Tool by SCCroix\n")
